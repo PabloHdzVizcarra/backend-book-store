b4aa7e404b1c82d6a3e8e64f918308d4bd4e7b8d
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-08-04 09:09:23 -0500

    feat now read the body to request format string

--- src/main/java/com/pablojvm/infrastructure/LoginHandler.java
+++ src/main/java/com/pablojvm/infrastructure/LoginHandler.java
@@ -4,12 +4,24 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 public class LoginHandler implements HttpHandler
 {
     @Override
     public void handle(HttpExchange exchange) throws IOException
     {
+        StringBuilder stringBuilder = new StringBuilder();
 
+        String requestMethod = exchange.getRequestMethod();
+        System.out.println(requestMethod);
+        InputStream requestBody = exchange.getRequestBody();
+        int i;
+        while ((i = requestBody.read()) != -1)
+        {
+            stringBuilder.append((char) i);
+        }
+
+        System.out.println(stringBuilder);
     }
 }
