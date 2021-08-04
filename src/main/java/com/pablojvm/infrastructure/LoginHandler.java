0b5398ad72cc899ba1e508049337e06d80adfe9c
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-08-04 10:15:09 -0500

    feat convert request json body to string

--- src/main/java/com/pablojvm/infrastructure/LoginHandler.java
+++ src/main/java/com/pablojvm/infrastructure/LoginHandler.java
@@ -1,5 +1,7 @@
 package com.pablojvm.infrastructure;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
@@ -11,17 +13,40 @@ public class LoginHandler implements HttpHandler
     @Override
     public void handle(HttpExchange exchange) throws IOException
     {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String requestMethod = exchange.getRequestMethod();
+
+        String requestBody = getRequestBody(exchange);
+
+        DataPostUser dataPostUser = objectMapper.readValue(
+                requestBody,
+                new TypeReference<DataPostUser>()
+                {
+                }
+        );
+
+        System.out.println(dataPostUser);
+
+    }
+
+    /**
+     * Convert the body request from JSON format to string format.
+     *
+     * @param exchange A {@link HttpExchange} with the HTTP method data.
+     * @return The body of the request in string format.
+     * @throws IOException if there is an error reading the body
+     */
+    private String getRequestBody(HttpExchange exchange) throws IOException
+    {
+        InputStream requestBody = exchange.getRequestBody();
         StringBuilder stringBuilder = new StringBuilder();
 
-        String requestMethod = exchange.getRequestMethod();
-        System.out.println(requestMethod);
-        InputStream requestBody = exchange.getRequestBody();
         int i;
         while ((i = requestBody.read()) != -1)
         {
             stringBuilder.append((char) i);
         }
 
-        System.out.println(stringBuilder);
+        return stringBuilder.toString();
     }
 }
