1a04a2a2561045a48a8d4ec01975e7847a30e2e0
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-08-04 08:24:59 -0500

    feat create basic server http

--- src/main/java/com/pablojvm/Main.java
+++ src/main/java/com/pablojvm/Main.java
@@ -1,9 +1,27 @@
 package com.pablojvm;
 
+import com.pablojvm.infrastructure.LoginHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+// TODO: 8/4/21 create basic http server
+// TODO: 8/4/21 create first endpoint post user
 public class Main
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
-        System.out.println("Hello Book Store App");
+        HttpServer server = HttpServer
+                .create(new InetSocketAddress("localhost", 8081), 0);
+        ThreadPoolExecutor threadPoolExecutor =
+                (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
+
+        server.createContext("/login", new LoginHandler());
+        server.setExecutor(threadPoolExecutor);
+        server.start();
+        System.out.println("server on in port 8081");
     }
 }
