a2403ff4a6f3d1cf88c8ca52e027bc7b9a7f7cea
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-08-05 09:49:53 -0500

    feat service create user to save in database

--- src/main/java/com/pablojvm/application/UserPersistenceService.java
+++ src/main/java/com/pablojvm/application/UserPersistenceService.java
@@ -1,6 +1,7 @@
 package com.pablojvm.application;
 
 import com.pablojvm.domain.DataPostUser;
+import com.pablojvm.domain.User;
 
 public class UserPersistenceService
 {
@@ -13,6 +14,13 @@ public class UserPersistenceService
     // TODO: 8/5/21 hash password
     public void saveUser(DataPostUser data)
     {
+        User userToSave = new User(
+                data.getName(),
+                data.getLastname(),
+                data.getEmail(),
+                data.getPassword()
+        );
+
 
     }
 }
