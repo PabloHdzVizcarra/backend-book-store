c635cd7cfa03c8d35a12397aa3d0f3dbf730ee04
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-08-07 12:54:18 -0500

    feat create LoginData

--- src/test/java/com/pablojvm/infrastructure/MapperImplTest.java
+++ src/test/java/com/pablojvm/infrastructure/MapperImplTest.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.platform.commons.annotation.Testable;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Testable
 class MapperImplTest {
@@ -35,5 +37,19 @@ class MapperImplTest {
 
         assertThrows(IllegalArgumentException.class, () ->
                 mapper.createLoginData(body));
+    }
+
+    @Test
+    void shouldCreateEmptyValues() {
+        JacksonImpl mapper = new JacksonImpl();
+        String body = "{\n" +
+                "  \"email\": \"\",\n" +
+                "  \"password\": \"\"\n" +
+                "}";
+
+        LoginData data = mapper.createLoginData(body);
+
+        assertEquals("", data.getEmail());
+        assertEquals("", data.getPassword());
     }
 }
