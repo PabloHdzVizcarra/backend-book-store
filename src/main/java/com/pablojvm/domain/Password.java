2d9c70768d5faf1164fcdfc2da46fd3d2febd010
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-08-08 10:02:20 -0500

    feat method to compare passwords

--- src/main/java/com/pablojvm/domain/Password.java
+++ src/main/java/com/pablojvm/domain/Password.java
@@ -3,11 +3,13 @@ package com.pablojvm.domain;
 import org.mindrot.jbcrypt.BCrypt;
 
 
-public class Password
-{
+public class Password {
 
-    public static String create(String password)
-    {
+    public static String create(String password) {
         return BCrypt.hashpw(password, BCrypt.gensalt(12));
+    }
+
+    public static boolean compare(String password, String hashPassword) {
+        return BCrypt.checkpw(password, hashPassword);
     }
 }
