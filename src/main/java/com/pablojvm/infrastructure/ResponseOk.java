990eb14abb53e5a2231ac209783ecaa51383c6e9
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-08-08 12:03:51 -0500

    feat create jwt correctly

--- src/main/java/com/pablojvm/infrastructure/ResponseOk.java
+++ src/main/java/com/pablojvm/infrastructure/ResponseOk.java
@@ -2,10 +2,34 @@ package com.pablojvm.infrastructure;
 
 import com.pablojvm.user.LoginData;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 import io.javalin.http.Context;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 
 public class ResponseOk {
     public void withCookie(Context context, LoginData dataRequest) {
+        Map<String, Object> passAndEmail = new HashMap<>();
+        passAndEmail.put("password", dataRequest.getPassword());
+        passAndEmail.put("email", dataRequest.getEmail());
+        byte[] secret = Base64.getDecoder().decode("tOX" +
+                "+BgAa4v1jS0Kjvs9gRtpdiNtWHwvekd7VgNUUJwo=");
+        Instant now = Instant.now();
+
+        String jwt = Jwts.builder()
+                .setSubject("admin")
+                .setHeader(passAndEmail)
+                .setExpiration(Date.from(now.plus(5, ChronoUnit.MINUTES)))
+                .signWith(Keys.hmacShaKeyFor(secret))
+                .compact();
+
+        System.out.println(jwt);
 
     }
 }
