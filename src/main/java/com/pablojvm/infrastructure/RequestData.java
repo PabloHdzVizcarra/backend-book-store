4e12b8adaa9481facf452f88b3b90a8c7bf05c53
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-08-11 08:19:52 -0500

    feat create object for represent all data shared

--- src/main/java/com/pablojvm/infrastructure/RequestData.java
+++ src/main/java/com/pablojvm/infrastructure/RequestData.java
@@ -1,6 +1,7 @@
 package com.pablojvm.infrastructure;
 
 import com.pablojvm.application.ValidationData;
+import com.pablojvm.domain.DataUser;
 import com.pablojvm.user.UserService;
 
 import java.util.logging.Logger;
@@ -8,12 +9,13 @@ import java.util.logging.Logger;
 import io.javalin.http.Context;
 
 public class RequestData {
-    private Context httpContext;
-    private Mapper mapper;
-    private ValidationData validationService;
-    private Logger logger;
-    private UserService userService;
-    private JwtService jwtService;
+    private final Context httpContext;
+    private final Mapper mapper;
+    private final ValidationData validationService;
+    private final Logger logger;
+    private final UserService userService;
+    private final JwtService jwtService;
+    private DataUser dataUser;
 
     public RequestData(
             Context httpContext,
@@ -21,7 +23,8 @@ public class RequestData {
             ValidationData validationService,
             Logger logger,
             UserService userService,
-            JwtService jwtService
+            JwtService jwtService,
+            DataUser dataUser
     ) {
         this.httpContext = httpContext;
         this.mapper = mapper;
@@ -29,6 +32,15 @@ public class RequestData {
         this.logger = logger;
         this.userService = userService;
         this.jwtService = jwtService;
+        this.dataUser = dataUser;
+    }
+
+    public DataUser getDataUser() {
+        return dataUser;
+    }
+
+    public void setDataUser(DataUser dataUser) {
+        this.dataUser = dataUser;
     }
 
     public Context getHttpContext() {
